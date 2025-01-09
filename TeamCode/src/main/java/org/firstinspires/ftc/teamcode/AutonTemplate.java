4501c3e1d563c7ef1e96699df5fc719f2e658171
Project516 <138796702+Project516@users.noreply.github.com>
2025-01-09 17:18:25 -0600

    Update AutonTemplate.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTemplate.java
@@ -1,50 +1,98 @@
 package org.firstinspires.ftc.teamcode;
 
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+
 @Autonomous
 public class AutonTemplate extends LinearOpMode{
-
-    private DcMotor back_left_motor;
-    private DcMotor back_right_motor;
-    private DcMotor front_left_motor;
-    private DcMotor front_right_motor;
-    private DcMotor shoulder;
-    private Servo claw;
-    private Servo wrist;
-    private DcMotor left_slide;
-    private DcMotor right_slide;
+    double LX, LY, RX, sensitivity = 0.5, a, b;
+    DcMotor BL, FL, FR, BR, S1, S2, M1, M2;
+    Servo Intake, Arm, Wrist;
+    private ElapsedTime runtime = new ElapsedTime();
+    public void MoveUntil(double speed, double time, String direction){
+        runtime.reset();
+        if (direction.equals("left")){
+            BL.setPower(speed);
+            FL.setPower(-speed);
+            FR.setPower(speed);
+            BR.setPower(-speed);
+        }
+        else if (direction.equals("right")){
+            BL.setPower(-speed);
+            FL.setPower(speed);
+            FR.setPower(-speed);
+            BR.setPower(speed);
+        }
+        else if (direction.equals("forward")){
+            BL.setPower(speed);
+            FL.setPower(speed);
+            FR.setPower(speed);
+            BR.setPower(speed);
+        }
+        else if (direction.equals("backward")){
+            BL.setPower(-speed);
+            FL.setPower(-speed);
+            FR.setPower(-speed);
+            BR.setPower(-speed);
+        }
+        while (runtime.seconds() < time && !isStopRequested()){}
+        BL.setPower(0);
+        FL.setPower(0);
+        FR.setPower(0);
+        BR.setPower(0);
+    }
+    public void Turn(double speed, double time){
+        runtime.reset();
+        BL.setPower(speed);
+        FL.setPower(speed);
+        FR.setPower(-speed);
+        BR.setPower(-speed);
+        while (runtime.seconds() < time && !isStopRequested()){}
+        BL.setPower(0);
+        FL.setPower(0);
+        FR.setPower(0);
+        BR.setPower(0);
+    }
     @Override
     public void runOpMode(){
-
-        front_left_motor = hardwareMap.get(DcMotor.class,("front_left_motor"));
-        front_right_motor = hardwareMap.get(DcMotor.class,("front_left_motor"));
-        back_left_motor = hardwareMap.get(DcMotor.class,("front_left_motor"));
-        back_right_motor = hardwareMap.get(DcMotor.class,("front_left_motor"));
-        left_slide = hardwareMap.get(DcMotor.class, ("left_slide"));
-        right_slide = hardwareMap.get(DcMotor.class, ("right_slide"));
-        shoulder = hardwareMap.get(DcMotor.class,("shoulder"));
-        wrist = hardwareMap.get(Servo.class, ("wrist"));
-        claw = hardwareMap.get(Servo.class, ("claw"));
-
-        front_left_motor.setDirection(DcMotorSimple.Direction.REVERSE);
-        right_slide.setDirection(DcMotorSimple.Direction.REVERSE);
-        left_slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        right_slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        BL = hardwareMap.dcMotor.get("leftBack");
+        FL = hardwareMap.dcMotor.get("leftFront");
+        FR = hardwareMap.dcMotor.get("rightFront");
+        BR = hardwareMap.dcMotor.get("rightBack");
+        S1 = hardwareMap.dcMotor.get("par1");
+        S2 = hardwareMap.dcMotor.get("par0");
+        S1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        S2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        S1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        S2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        S1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        S2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // This matches the DcMotors to the ones configured in the Control Hub
+        FL.setDirection(REVERSE);
+        S2.setDirection(REVERSE);
+        Intake = hardwareMap.get(Servo.class, "claw");
+        Arm = hardwareMap.get(Servo.class, "shoulder");
+        Wrist = hardwareMap.get(Servo.class, "wrist");
 
         waitForStart();
-
-        back_left_motor.setPower(.3);
-        back_right_motor.setPower(.3);
-        front_left_motor.setPower(.3);
-        front_right_motor.setPower(.3);
-        claw.setPosition(0);
-
-        sleep(1000);
 
     }
 }
