5de3a1d9ccf66c0a051618e1d2b40e1398293052
KrisahaanDesai <krishaandesai@icloud.com>
2025-02-14 17:57:48 -0600

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Actions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Actions.java
@@ -11,27 +11,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import org.jetbrains.annotations.NotNull;
 
-
-@Autonomous
+@Autonomous(name = "Auton_Actions")
 public class Auton_Actions extends LinearOpMode {
 
     private DcMotorEx slide1, slide2;
     private Servo claw;
     private CRServo wrist;
     private long startTime;
+
     private static final long SPIN_UP_TIME = 3000;
     private static final long SLIDES_DOWN_TIME = 3000;
     private static final long ELBOW_EXTEND_TIME = 500;
     private static final long ELBOW_RETRACT_TIME = 500;
 
-    private MecanumDrive drive; // Declare drive as a class member
-    private Trajectory trajectory; // Declare trajectory as a class member
-
+    private MecanumDrive drive;
+    private Pose2d initialPose;
 
     @Override
     public void runOpMode() {
@@ -40,13 +38,10 @@ public class Auton_Actions extends LinearOpMode {
         claw = hardwareMap.get(Servo.class, "claw");
         wrist = hardwareMap.get(CRServo.class, "wrist");
 
-        Pose2d initialPose = new Pose2d(-1.37, 1.58, Math.toRadians(-144.9));
-        drive = new MecanumDrive(hardwareMap, initialPose); // Initialize drive
+        initialPose = new Pose2d(-1.37, 1.58, Math.toRadians(-144.9));
+        drive = new MecanumDrive(hardwareMap, initialPose);
 
         wrist.setDirection(CRServo.Direction.FORWARD);
-
-        // Build trajectory *before* waitForStart
-
 
         waitForStart();
 
@@ -117,7 +112,7 @@ public class Auton_Actions extends LinearOpMode {
         @Override
         public boolean run(@NotNull TelemetryPacket telemetryPacket) {
             claw.setPosition(0.5);
-            return true; // Return true as this action is instantaneous
+            return true;
         }
     }
 
@@ -143,10 +138,14 @@ public class Auton_Actions extends LinearOpMode {
 
     public Action strafeToLineHeading() {
         return new Action() {
+            private TrajectoryActionBuilder tab3;
+
             @Override
             public boolean run(@NotNull TelemetryPacket telemetryPacket) {
-                TrajectoryActionBuilder tab3 = drive.actionBuilder(initialPose).strafeTo(new Vector2d(46, 30));
-
+                if (tab3 == null) {
+                    tab3 = drive.actionBuilder(initialPose).strafeTo(new Vector2d(46, 30));
+                }
+                return Actions.run(tab3);
             }
         };
     }
