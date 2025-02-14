9338dce1966f11398b428da1ae4679250bd5289d
KrisahaanDesai <krishaandesai@icloud.com>
2025-02-14 14:14:06 -0600

    Update Auton_Actions.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Actions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Actions.java
@@ -2,30 +2,25 @@ package org.firstinspires.ftc.teamcode;
 
 import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
-import com.acmerobotics.roadrunner.ftc.Actions;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.acmerobotics.roadrunner.Action;
 
 @Autonomous
-public class Auton_Actions {
+public class Auton_Actions extends OpMode {
 
     public DcMotorEx slide1, slide2;
     public Servo claw;
     private long startTime;
     private static final long SPIN_UP_TIME = 2000; // Wait time in milliseconds (2 seconds)
 
-    public Auton_Actions(HardwareMap hardwareMap) {
-        slide1 = hardwareMap.get(DcMotorEx.class, "par0");
-        slide2 = hardwareMap.get(DcMotorEx.class, "par1");
-        claw = hardwareMap.get(Servo.class, "claw");
-    }
-
+    // Action classes
     public class SpinUp implements Action {
         private boolean initialized = false;
 
@@ -75,8 +70,19 @@ public class Auton_Actions {
         return new UnGrab();
     }
 
-    // Run all actions in sequence
-    public void runAllActions() {
+    @Override
+    public void init() {
+        // Initialize hardware
+        slide1 = hardwareMap.get(DcMotorEx.class, "par0");
+        slide2 = hardwareMap.get(DcMotorEx.class, "par1");
+        claw = hardwareMap.get(Servo.class, "claw");
+
+        // Initialize any other configurations (like motors, sensors, etc.)
+    }
+
+    @Override
+    public void loop() {
+        // Run the sequence of actions
         Actions.runBlocking(new SequentialAction(
                 spinUp(),  // Spin up motor
                 grab(),    // Grab the object
